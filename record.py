2f1d57f40abe325a041f942c99b37faeaf14f83f
BeckyHeath <r.heath18@imperial.ac.uk>
2024-03-24 13:49:17 +0000

    differnt backend

--- record.py
+++ record.py
@@ -12,7 +12,7 @@ import datetime
 # Function to capture picture from camera
 def capture_picture(camera_index, save_dir):
     # Open camera
-    camera = cv2.VideoCapture(camera_index, cv2.CAP_MSMF)
+    camera = cv2.VideoCapture(camera_index, cv2.CAP_GSTREAMER)
     if not camera.isOpened():
         print(f"Error: Unable to open camera {camera_index}")
         return
