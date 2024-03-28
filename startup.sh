4fa9a14804710b423b27be1f1ae08a10d77d61f5
BeckyHeath <r.heath18@imperial.ac.uk>
2024-03-28 14:25:52 +0000

    Simplify shell script

--- startup.sh
+++ startup.sh
@@ -2,18 +2,11 @@
 
 # Startup script to run record.py 
 
-
-# Activate the venv: 
-source /home/bugs/.venv/bin/activate > /home/bugs/Desktop/AutoBeetle-BeetleMonitoring/logfile.log 2>&1 &
-
-
-# Move to the project github
-cd /home/bugs/Desktop/AutoBeetle-BeetleMonitoring/ > /home/bugs/Desktop/AutoBeetle-BeetleMonitoring/logfile.log 2>&1 &
-
+# Activate the venv and move to the project directory
+source /home/bugs/.venv/bin/activate && cd /home/bugs/Desktop/AutoBeetle-BeetleMonitoring/ 
 
 # Run your record.py and append outputs to log file 
-python3 record.py  >> logfile.log 2>&1 &
-
+python3 record.py >> /home/bugs/Desktop/AutoBeetle-BeetleMonitoring/logfile.log 2>&1 &
 
 # Display the contents of the log file in a terminal window
-x-terminal-emulator -e "tail -f logfile.log"
+x-terminal-emulator -e "tail -f /home/bugs/Desktop/AutoBeetle-BeetleMonitoring/logfile.log"
