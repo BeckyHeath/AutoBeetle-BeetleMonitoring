3c023c6ca40d70f021887ab2e3e8f15eecd7b990
BeckyHeath <r.heath18@imperial.ac.uk>
2024-03-28 14:39:14 +0000

    remove logfile stuff

--- startup.sh
+++ startup.sh
@@ -6,7 +6,6 @@
 source /home/bugs/.venv/bin/activate && cd /home/bugs/Desktop/AutoBeetle-BeetleMonitoring/ 
 
 # Run your record.py and append outputs to log file 
-python3 record.py >> /home/bugs/Desktop/AutoBeetle-BeetleMonitoring/logfile.log 2>&1 &
+python3 record.py 
 
-# Display the contents of the log file in a terminal window
-x-terminal-emulator -e "tail -f /home/bugs/Desktop/AutoBeetle-BeetleMonitoring/logfile.log"
+
