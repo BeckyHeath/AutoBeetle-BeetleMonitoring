e295aae063d11d59ca6a91c12d0d6e2a5b202543
BeckyHeath <r.heath18@imperial.ac.uk>
2024-03-30 08:20:49 +0000

    simplify

--- startup.sh
+++ startup.sh
@@ -3,7 +3,9 @@
 # Startup script to run record.py 
 
 # Activate the venv and move to the project directory
-source /home/bugs/.venv/bin/activate && cd /home/bugs/Desktop/AutoBeetle-BeetleMonitoring/ 
+source /home/bugs/.venv/bin/activate
+
+cd /home/bugs/Desktop/AutoBeetle-BeetleMonitoring/ 
 
 # Run your record.py and append outputs to log file 
 python3 record.py 
